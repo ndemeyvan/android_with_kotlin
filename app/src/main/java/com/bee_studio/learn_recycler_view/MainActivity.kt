1d635aec7d7a869a923cd454d20bab467c06ddf7
ndemeyvan <ndemeyvan@gmail.com>
2021-10-15 22:28:39 +0200

    introduction to LiveData in kotlin

--- app/src/main/java/com/bee_studio/learn_recycler_view/MainActivity.kt
+++ app/src/main/java/com/bee_studio/learn_recycler_view/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.bee_studio.learn_recycler_view.BasicViewModel.BasicViewModel
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var button : Button;
-   private lateinit var countText : TextView;
+    private lateinit var button: Button;
+    private lateinit var countText: TextView;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +19,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.button)
         countText = findViewById(R.id.textView)
+        //CountDown timer with liveData
+        //1-Cree une instance du ViewModel
         var viewModel = ViewModelProvider(this).get(BasicViewModel::class.java)
-        countText.text=viewModel.num.toString()
-
+        //2- Observer et recuperer la valeur de cette LiveData
+        viewModel.seconds().observe(this, {
+            countText.text = it.toString()
+        })
+        //2- Observer et recuperer la valeur de cette LiveData
+        viewModel.finished.observe(this, {
+            if (it) {
+                Toast.makeText(applicationContext, "The counter is finish", Toast.LENGTH_LONG)
+                    .show()
+            }
+        })
         button.setOnClickListener {
-            viewModel.addNumber()
-            countText.text=viewModel.num.toString()
+            viewModel.startTimer()
         }
+
 
     }
 
